8dc1053900732e06dd9d3ab6bb30940e0d672d26
Vsevolod Volkov <st.lyn4@gmail.com>
2021-03-16 00:31:19 +0200

    Menu "Info" without a description

--- src/screens/Info.js
+++ src/screens/Info.js
@@ -1,4 +1,45 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { ScrollView, View, StyleSheet } from 'react-native';
+import { Avatar, Title, Caption, Paragraph } from 'react-native-paper';
 
-export default () => <Text>Info</Text>;
+import logo from 'app/assets/images/logo/icon.png';
+
+export default function Info() {
+  return (
+    <ScrollView style={styles.infoContent}>
+      <Avatar.Image source={logo} size={50} style={styles.logo} />
+      <Title style={styles.title}>Randomix</Title>
+      <Caption style={styles.caption}>by Vsevolod Volkov</Caption>
+      <View style={styles.description}>
+        <Paragraph style={styles.paragraph}>
+          TODO: нормальное описание проекта
+        </Paragraph>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  infoContent: {
+    flex: 1,
+    marginTop: 10,
+    paddingLeft: 20,
+  },
+  logo: {
+    backgroundColor: '#989898',
+  },
+  title: {
+    marginTop: 20,
+    fontWeight: 'bold',
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 14,
+  },
+  description: {
+    marginTop: 20,
+  },
+  paragraph: {
+    fontWeight: 'bold',
+  },
+});
